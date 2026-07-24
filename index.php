295194597813d95899b4f5d18928736afb12c623
Boivana <boivanaofficial@gmail.com>
2026-07-24 23:33:44 +0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Boivana</title>
+</head>
+<body>
+    <h1>Welcome to Boivana 📚</h1>
+    <p>Website is under development...</p>
+</body>
+</html>
