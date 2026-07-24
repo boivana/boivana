57db4060b50322cff0f3438b9d551e55908ab4b8
Boivana <boivanaofficial@gmail.com>
2026-07-25 00:22:17 +0600

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,3 +22,51 @@ cartButtons.forEach(button => {
         alert("Book added to cart! Total books: " + cartCount);
     });
 });
+// Book Search System
+
+const searchInput = document.querySelector("#search");
+
+if(searchInput){
+    searchInput.addEventListener("keyup", function(){
+        let value = searchInput.value.toLowerCase();
+
+        let books = document.querySelectorAll(".book-card");
+
+        books.forEach(book => {
+            let title = book.innerText.toLowerCase();
+
+            if(title.includes(value)){
+                book.style.display = "block";
+            }else{
+                book.style.display = "none";
+            }
+        });
+    });
+}
+
+
+// Category Filter
+
+const categoryButtons = document.querySelectorAll(".category-btn");
+
+categoryButtons.forEach(btn => {
+    btn.addEventListener("click", () => {
+
+        let category = btn.innerText.toLowerCase();
+
+        let books = document.querySelectorAll(".book-card");
+
+        books.forEach(book => {
+
+            let bookCategory = book.dataset.category;
+
+            if(category === "all" || bookCategory === category){
+                book.style.display = "block";
+            }else{
+                book.style.display = "none";
+            }
+
+        });
+
+    });
+});
